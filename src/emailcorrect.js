da6886ee340896e4dd9e6917973f2d4258e4e0f8
Jesse Dijkstra <jesse@blendle.com>
2016-06-27 17:26:38 +0200

    Add JSDoc

--- src/emailcorrect.js
+++ src/emailcorrect.js
@@ -1,8 +1,19 @@
 import { TLD_MISTAKES, HOSTNAME_MISTAKES } from '../config/mistakes';
 
 /**
+ * @typedef DomainDisect
+ * @type Object
+ * @property {string} hostname The hostname part of the e-mail address
+ * @property {string} tld The TLD part of the e-mail address
+ */
+
+/**
+ * Dissects the given domain string into hostname and top level domain parts.
+ *
+ * @param  {string} [domain='']
+ * @return {DomainDisect}
+ */
 function disectDomain(domain = '') {
-  // Get TLD and hostname from domain.
   const parts = domain.split('.');
   const tld = parts.pop();
   const hostname = parts.join('.');
@@ -10,18 +21,48 @@ function disectDomain(domain = '') {
   return { hostname, tld };
 }
 
+/**
+ * @typedef EmailDisect
+ * @type Object
+ * @property {string} user The user part of the e-mail address
+ * @property {string} hostname The hostname part of the e-mail address
+ * @property {string} tld The TLD part of the e-mail address
+ */
+
+/**
+ * Dissects the given email string into user, domain and top level domain parts.
+ *
+ * @param  {string} email
+ * @return {EmailDisect}
+ */
 function disect(email) {
-  // Split up into user and domain.
   const [user, domain] = (email || '').split('@');
   const { hostname, tld } = disectDomain(domain);
 
   return { user, hostname, tld };
 }
 
+/**
+ * Compiles the new e-mail address
+ *
+ * @param  {string} user
+ * @param  {string} hostname
+ * @param  {string} tld
+ * @return {string}
+ */
 function compile(user, hostname, tld) {
   return `${user}@${hostname}.${tld}`;
 }
 
+/**
+ * Autocorrects the input value bsaed on the solutions given as the first argument.
+ *
+ * @example
+ * autoCorrect({ gmail: ['gmai', 'gnail'] }, 'gnail'}); // Returns gmail
+ * @param  {[type]} mistakes [description]
+ * @param  {[type]} input    [description]
+ * @return {[type]}          [description]
+ */
 function autoCorrect(mistakes, input) {
   return Object.keys(mistakes).reduce((acc, solution) => {
     const mistakesForSolution = mistakes[solution];
@@ -34,6 +75,13 @@ function autoCorrect(mistakes, input) {
   }, input);
 }
 
+/**
+ * Creates a suggestion e-mail for the input. Returns an empty string for invalid
+ * values. Valid input values are string and number.
+ *
+ * @param  {(String|Number)} email
+ * @return {String}
+ */
 export function suggest(email) {
   if (typeof email !== 'string' && typeof email !== 'number') {
     return '';
