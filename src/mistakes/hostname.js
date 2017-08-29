593454976928d1a78a7cff85fb0eb2af52bba108
Mathieu Heruer <mathieu@biteweb.nl>
2017-08-29 14:54:37 +0200

    Add typo (#6)

--- src/mistakes/hostname.js
+++ src/mistakes/hostname.js
@@ -1,5 +1,5 @@
 module.exports = {
-  gmail: ['gmai', 'gail', 'gmai', 'gnail', 'mgail', 'gmial', 'mgail', 'gamil'],
+  gmail: ['gmai', 'gail', 'gmai', 'gnail', 'mgail', 'gmial', 'mgail', 'gamil', 'gmal'],
   hotmail: [
     'htmail', 'otmail', 'hotmal', 'hotmai', 'hotmial', 'hoitmail', 'homail',
     'hotrmail', 'hotmil', 'hotmaill',
